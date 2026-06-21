370fdafd138450f2532b90ad004352ffc831b8f3
Aditya Singh <2k22.csai.32131@gmail.com>
2026-06-21 20:06:21 +0000

    Add migration route and startup migration

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,3 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskController;
 
 Route::apiResource('tasks', TaskController::class);
+use Illuminate\Support\Facades\DB;
+
+Route::get('/debug-db', function () {
+    return [
+        'tables' => DB::select("SELECT name FROM sqlite_master WHERE type='table'")
+    ];
+});
+
+Route::get('/run-migrate', function () {
+    \Artisan::call('migrate', ['--force' => true]);
+
+    return [
+        'output' => \Artisan::output()
+    ];
+});	
