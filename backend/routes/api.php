f1f415078ba0171393387e1ac77f3e91359fcdc5
Aditya Singh <2k22.csai.32131@gmail.com>
2026-06-22 04:25:56 +0000

    Remove debug routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,18 +4,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskController;
 
 Route::apiResource('tasks', TaskController::class);
-use Illuminate\Support\Facades\DB;
-
-Route::get('/debug-db', function () {
-    return [
-        'tables' => DB::select("SELECT name FROM sqlite_master WHERE type='table'")
-    ];
-});
-
-Route::get('/run-migrate', function () {
-    \Artisan::call('migrate', ['--force' => true]);
-
-    return [
-        'output' => \Artisan::output()
-    ];
-});	
